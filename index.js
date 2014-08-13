675bc7f97b88a8eb0c67e6f3e7671f361aed3dfe
maddigger00009 <maddigger00009@maxctech.co.uk>
2014-08-13 13:37:18 +0100

    made files pritty

--- index.js
+++ index.js
@@ -3,45 +3,42 @@ $(document).ready(function () {
 		isok=true;
 		$('.input').removeClass("input-error")
 		if($("input[name='EMAIL']").val().length==0){
-		$("input[name='EMAIL']").addClass("input-error");
-		isok=false;
-		
+			$("input[name='EMAIL']").addClass("input-error");
+			isok=false;
 		}
 		if($("input[name='LNAME']").val().length==0){
 				$("input[name='LNAME']").addClass("input-error");
 				isok = false;
 		}
 		if($("input[name='FNAME']").val().length==0){
-					$("input[name='FNAME']").addClass("input-error");
-
-				isok = false;
-
+			$("input[name='FNAME']").addClass("input-error");
+			isok = false;
 		}
 		if(isok){
-		$.ajax({
-        type: "POST",
-        url: "http://care2save.us8.list-manage1.com/subscribe/post-json?u=f8a1f0d6a44f3a522008bfc32&id=b5d5e1a083&c=?",
-        data: $('.email').serialize(),
-        cache       : false,
-        dataType    : 'json',
-        contentType: "application/json; charset=utf-8",
-        error       : function(err) { console.log(err) },
-        success     : function(data) {
-            if (data.result != "success") {
-		$('.error').text(data.msg.substring(3));
-		console.log(data);
-		$("input[name='EMAIL']").addClass("input-error");
-		            } else {
-		            			$('.email').hide();
-		$('.submit').hide();
-		$('.form').find('h1').text("Thank you");
-		$('.form').append("<p>"+data.msg+"</p>");
-		$('.error').hide();
-            }
-        }
-    });
-	}else{
-		$('.error').text("Some fields are empty");
-	}
+			$.ajax({
+        		type: "POST",
+        		url: "http://care2save.us8.list-manage1.com/subscribe/post-json?u=f8a1f0d6a44f3a522008bfc32&id=b5d5e1a083&c=?",
+        		data: $('.email').serialize(),
+        		cache       : false,
+        		dataType    : 'json',
+        		contentType: "application/json; charset=utf-8",
+        		error       : function(err) { console.log(err) },
+        		success     : function(data) {
+        	    	if (data.result != "success") {
+						$('.error').text(data.msg.substring(3));
+						console.log(data);
+						$("input[name='EMAIL']").addClass("input-error");
+	            	} else {
+           				$('.email').hide();
+						$('.submit').hide();
+						$('.form').find('h1').text("Thank you");
+						$('.form').append("<p>"+data.msg+"</p>");
+					$('.error').hide();
+            		}
+        		}
+    		});
+		}else{
+			$('.error').text("Some fields are empty");
+		}
 	});
 });
